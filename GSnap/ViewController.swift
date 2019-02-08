f7a364e2ff5f05f53633019dfd643264bed844b2
(thatnaoki) <muroya.naoki@gmail.com>
2019-02-08 10:38:55 +0900

    Initial Commit

---
+++ GSnap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GSnap
+//
+//  Created by Naoki Muroya on 2019/02/08.
+//  Copyright © 2019 thatnaoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
